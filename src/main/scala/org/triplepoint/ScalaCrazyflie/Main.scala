498be98c3f909fbb2923be21514cc22dcbc5a24d
Jonathan Hanson <jonathan@jonathan-hanson.org>
2014-01-18 23:02:41 -0800

    Use filter instead of for/yield in the search for the usb device

--- src/main/scala/org/triplepoint/ScalaCrazyflie/Main.scala
+++ src/main/scala/org/triplepoint/ScalaCrazyflie/Main.scala
@@ -1,14 +1,11 @@
 package org.triplepoint.ScalaCrazyflie
 
 object Main extends App {
-  println("test")
-
   val radio = CrazyRadio()
 
+  // Testing
   val desc = radio.usbDevice.javaUsbDevice.getUsbDeviceDescriptor
-
   println(f"${desc.idVendor() & 0xffff}%04x:${desc.idProduct() & 0xffff}%04x")
-
 }
 
 
--- src/main/scala/org/triplepoint/ScalaCrazyflie/UsbDevice.scala
+++ src/main/scala/org/triplepoint/ScalaCrazyflie/UsbDevice.scala
@@ -11,7 +11,6 @@ object UsbDevice {
    */
   def apply(vendorId: Int, productId: Int) = {
     def flattenListOfUsbDevices(device: JavaxUsbDevice): List[JavaxUsbDevice] = {
-      recursiveFlattenListOfUsbDevices(List(device))
 
       // This helps us deal with the List that UsbHub.getAttachedUsbDevices() returns.
       implicit def javaListToUsbDeviceList(x: java.util.List[_]): List[JavaxUsbDevice] =
@@ -22,14 +21,16 @@ object UsbDevice {
           case x: JavaxUsbDevice if x.isUsbHub => x :: recursiveFlattenListOfUsbDevices(x.asInstanceOf[UsbHub].getAttachedUsbDevices)
           case x: JavaxUsbDevice               => List(x)
         }
+
+      recursiveFlattenListOfUsbDevices(List(device))
     }
 
     val rootHub = UsbHostManager.getUsbServices.getRootUsbHub
-    val foundDevice :: restOfList = for (
-      device <- flattenListOfUsbDevices(rootHub)
-      if device.getUsbDeviceDescriptor.idVendor == vendorId
-      if device.getUsbDeviceDescriptor.idProduct == productId
-    ) yield device
+    val foundDevice = flattenListOfUsbDevices(rootHub).filter(x => {
+        val desc = x.getUsbDeviceDescriptor
+        (desc.idVendor == vendorId) && (desc.idProduct == productId)
+      }
+    ).head
 
     // TODO - what happens here if nothing is found?  Option?
 
