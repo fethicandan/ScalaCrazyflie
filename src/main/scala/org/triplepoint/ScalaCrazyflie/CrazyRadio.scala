5f6d7d590a8baf1585656c396fc6a8726e159c0d
Jonathan Hanson <jonathan@jonathan-hanson.org>
2014-01-18 22:53:59 -0800

    Setting the radio's product Id to be the right value.

--- src/main/scala/org/triplepoint/ScalaCrazyflie/CrazyRadio.scala
+++ src/main/scala/org/triplepoint/ScalaCrazyflie/CrazyRadio.scala
@@ -2,7 +2,7 @@ package org.triplepoint.ScalaCrazyflie
 
 object CrazyRadio {
   val vendorId = 0x1915
-  val productId = 0x7776
+  val productId = 0x7777
 
   def apply() = new CrazyRadio(UsbDevice(vendorId, productId))
 }
